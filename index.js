a87765e1ffdd6f9f4fe570c73d65b363705dac31
BearmanCodes <idostuff1225@gmail.com>
2021-08-20 14:50:39 -0400

    Added most of it
    Add node-mailer when you get home

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 const url = "https://www.newegg.com/product-shuffle";
+require('dotenv').config();
 
 (async () => {
     const browser = await puppeteer.launch( {headless: false, args: ['--start-maximized']} );
@@ -7,7 +8,20 @@ const url = "https://www.newegg.com/product-shuffle";
     await page.setViewport({ width: 1440, height: 900});
     //page.setRequestInterception(true);
     await page.goto(url);
-    await page.type('#labeled-input-signEmail', "test");
-    //await page.screenshot( {path: "C:/Users/User/Documents/GitHub/Newegg-Scraper/output.png"} );
-    //await browser.close();
+    await page.type('#labeled-input-signEmail', `${process.env['EMAIL']}`);
+    await page.click('#signInSubmit');
+    await page.waitForSelector('#labeled-input-password');
+    await page.type('#labeled-input-password', `${process.env['PASSWORD']}`);
+    await page.click("#signInSubmit");
+    await page.waitForSelector(".modal-content");
+    await page.waitForSelector(".close");
+    await page.click(".close");
+    await page.waitForSelector(".bg-wide-flag");
+    var element = await page.$(".bg-wide-flag");
+    var text = await element.evaluate(element => element.textContent);
+    if (text == " Event Starts In"){
+        text = await element.evaluate(element => element.textContent);
+    }
+    
+    console.log(text);
 }) ()
